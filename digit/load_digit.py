8ed91c3371ce55d21efe75674b19494c8c896195
tomMoral <tomamoral@gmail.com>
2013-05-28 18:00:18 -0700

    Setup an environment for the digit recognition competition
    To start, run the following command in your repo:
        sudo python setup.py develop
    
    You can then use the digit package functionnalities calling
        import digit
    in your programs.
    
    You have to download the training data (train.csv) in the digit/
    directory.
    
    To develop, place the basic functions in the digit package
    and wrap it up in scripts written in the scripts/ folder.
    
    Don't forget to comment your code with docstring for a better
    reuse.

---
+++ digit/load_digit.py
@@ -0,0 +1,14 @@
+
+
+def load(n_digit=-1):
+    import os
+
+    data = []
+    digit = []
+    train = os.path.join(os.path.dirname(__file__), "train.csv")
+    with open(train) as f:
+        for line in f.readlines()[1:n_digit+1]:
+            pix = line.split(',')
+            digit.append(pix[0])
+            data.append(pix[1:])
+    return digit, data
---
+++ scripts/run_digit.py
@@ -0,0 +1,7 @@
+#*------------------------------------------------
+#   tomMoral
+
+
+if __name__ == '__main__':
+    import digit.load_digit as dg
+    dataset = dg.load()
