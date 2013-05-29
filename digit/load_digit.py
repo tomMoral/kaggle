d518545a49c7d4f50ae4fcce514a12c68b949090
tomMoral <tomamoral@gmail.com>
2013-05-28 19:00:09 -0700

    Sklearn test

--- digit/load_digit.py
+++ digit/load_digit.py
@@ -7,8 +7,8 @@ def load(n_digit=-1):
     digit = []
     train = os.path.join(os.path.dirname(__file__), "train.csv")
     with open(train) as f:
-        for line in f.readlines()[1:n_digit+1]:
+        for line in f.readlines()[1:n_digit]:
             pix = line.split(',')
             digit.append(pix[0])
             data.append(pix[1:])
-    return digit, data
+    return (digit, data)
--- scripts/run_digit.py
+++ scripts/run_digit.py
@@ -3,5 +3,19 @@
 
 
 if __name__ == '__main__':
+    import numpy as np
+
+    from sklearn.multiclass import OneVsRestClassifier
+    from sklearn.svm import LinearSVC
+
     import digit.load_digit as dg
-    dataset = dg.load()
+
+    dataset = dg.load(1000)
+    print dataset[0]
+    lab = OneVsRestClassifier(LinearSVC(random_state=0)).fit(dataset[1], dataset[0]).predict(dataset[1])
+    print lab
+    count = np.zeros((10, 10))
+    for i, l in enumerate(lab):
+        count[l][int(dataset[0][i])] += 1
+    print count.nonzero()
+    raw_input("Press Enter to quit...")
