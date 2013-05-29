7b928cdfb25710f31f8bf1051d19f63540284fab
tomMoral <tomamoral@gmail.com>
2013-05-28 18:07:43 -0700

    Add project management files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import setuptools
+
+setup(name='Kaggle',
+      version='0.1',
+      description='Kaggle competition scripts',
+      author='Toyaume',
+      author_email='thomas.moreau.2010@gmail.com',
+      url='',
+      packages=['digit']
+      )
